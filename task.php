6ccea4af78cc30539a4cbc3144eef474563d68b5
liza-23 <jannatulfardus1243@gmail.com>
2025-11-02 15:05:08 +0600

    Task

---
+++ task.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Clinic Registration Form
+</title>
+
+
+<style>
+    body{
+        margin=10px;
+        padding=5px;
+        height: 100vh;
+    }
+
+    .h1{
+
+    }
+
+    
+
+
+
+    
+
+</style>
+</head>
+
+<body>
+
+    <center>
+        <h1 style="color:blue; font-family:times new roman;">Clinic Paitent Registation</h1>
+
+
+        <table>
+           
+
+        <tr><td>
+            <label>Full Name:</label>
+</td><td>
+    <input type="text"></td>
+</tr>
+       <tr><td>
+            Age:
+</td><td>
+    <input type="text"></td>
+</tr>
+       <tr><td>
+            Phone Number:
+</td><td>
+    <input type="text"></td>
+</tr>
+       <tr><td>
+            <label>Email Address:</label>
+</td><td>
+    <input type="text"> </td>
+</tr>
+       <tr><td>
+            Insurance Provider:
+</td><td>
+    <select>
+        <option>Select Provider</option>
+        <option>Pragati Insurance Limited</option>
+        <option>Islami Insurance Bangladesh></option>
+        <option>Meghna Life Insurance Bangladesh></option>
+
+        <tr><td>
+            Insurance Policy Number:
+</td><td>
+    <input type="text"></td>
+</tr>
+</body>
+
+
+
+
+
+
+
+        
+
+
+
+    </select>
+    </td>
+</tr>
+
+        </table>
+
+    </center>
+
+    
+    
+</body>
+<body>
+    <center>
+    <h1 style="color:blue; font-family:times new roman;">Additional Information</h1>
+
+     <table>
+
+        <tr><td>
+        User Name:
+</td><td>
+    <input type="text"></td>
+</tr>
+
+ 
+
+        <tr><td>
+            Password:
+</td><td>
+    <input type="text"></td>
+</tr>
+
+ 
+
+        <tr><td>
+            Confirm Password:
+</td><td>
+    <input type="text"></td>
+</tr>
+
+</table>
+
+</center>
+</body>
